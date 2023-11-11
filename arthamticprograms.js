51f8360adb2c102a25dd3d739706a0da73e46437
sesha <sesha472@gmail.com>
2023-11-11 20:46:31 +0530

    new  are added for practice more in js

---
+++ arthamticprograms.js
@@ -0,0 +1,81 @@
+// //  findnig factorial 
+
+
+// var n=3;
+// var fact=1;
+// for(var i=1; i<=n; i++){
+//  fact= fact*i;
+// }
+// console.log(fact);
+
+// using function 
+// function fact(n){
+//   var fact=1;
+//   for(var i=1; i<=n; i++){
+//    fact= fact*i;
+//   }
+//   return fact;
+
+// }
+// console.log(fact(6))
+
+
+
+// function fact(n){
+// if(n==0){
+//   return 1;
+// }
+// return n*fact(n-1);
+
+// }
+// console.log(fact(5))
+
+//  fibonassi program 
+
+//  var n=10;
+// var a=0,b=1;
+// var c=0;
+// for(i=0; i<=n;i++){
+//   console.log(c);
+// c=a+b;
+// a=b;
+// b=c;
+
+// }
+
+var n=3;
+var x=0,y=1,z=0
+
+for(i=0; i<=n; i++){
+  z=x+y;
+  x=z;
+  
+  
+}
+
+
+
+//
+//  function fibo(n){
+//   var c,a=0,b=1;
+//   for(i=1; i<=n; i++){
+//     c=a+b;
+//     a=b;
+//     b=c;
+//  console.log(c);
+// }
+// console.log("\n");
+//     return " ";
+// }
+// console.log(fibo(9));
+
+
+
+//   fib(n){
+//  if(n==0 || n==1){
+//   return 1;
+//   }
+//   t = fib(n-1)+fib(n-2);
+//   return t;
+// }
+//  console.log(fib(9));
