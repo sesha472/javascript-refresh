8620ffb9de99187f970d8f7465d08efd07a64554
seshasai <sesha472@gmail.com>
2024-03-11 23:41:02 +0530

    practice code2

--- arrmethods.js
+++ arrmethods.js
@@ -50,7 +50,9 @@
 
 
 
-
+// const a=[5,7,3,9,36];
+// let res=a.forEach((item,index)=>console.log(item))//undefinde because foreatch wont return anything we can do operations on eatch elemnt in arry
+// console.log(res);
 
 
 
@@ -58,11 +60,24 @@
 // let   arr2=[];
 
 // let result=arr1.entries();
-// for (const iterator of result) {
-//     arr2.push(iterator);
+// for (const [key,value] in arr1) {
+//     arr2.push(key);
 
 // }
 // console.log(arr2);
+
+//  sorting array elemnts 
+
+// let point = [30, 200, 2, 3, 15, 10];
+// let b=point.sort((a,b)=>{return b-a});
+// console.log(b);
+
+
+// let a=[3,45,767,5,4];
+// console.log(a.sort(function(a,b){
+//   return a-b;
+// }));
+
 
 // var array=['s','a','i','k','u','m','a','r'];
 // for(let i of array.keys()){
@@ -92,3 +107,191 @@
 // console.log(a.includes(5));
 // var array=['s','a','i','k','u','m','a','r'];
 // console.log(array.join(""));
+
+
+
+
+
+
+
+
+
+
+// let obj1={fname:"sachin",lname: "tendulkar",age:"27"};
+
+// function fullname({fname,lname}){
+//     console.log(fname);
+//     console.log(lname);
+// }
+
+// fullname(obj1)
+
+// console.log("Hellow" == "Hellow");
+// function handleTimeout(){
+//     console.log("normal function");
+// }
+
+// const handleTimeout2=()=>{
+//     console.log("arrow function..");
+
+
+// }
+//   setTimeout(handleTimeout,2000);
+// //   setTimeout(handleTimeout2,3000);
+// // setTimeout(() => {
+// //     console.log("set timeout function...");
+// // },5000 );
+
+// function init (){
+//     function greet (){
+//         console.log("hi");
+//     }
+//     return greet();
+// }
+// init();
+
+// const printName = (personObj) => {
+//     console.log(personObj.name);
+// }
+// printName({name: 'Max', age: 28}); // prints 'Max'
+
+
+// this is how distructuring works 
+// const printName = ({name}) => {
+//     console.log(name);
+// }
+// printName({name: 'Max', age: 28}); // prints 'Max')
+
+
+// Truncate a string to a certain number of words
+// truncateWithWordLimit("JavaScript is simple", 3) returns "JavaScript is simple"
+// const str = 'JavaScript is simple but not easy to master';
+// const wordLimit = 3;
+
+// let array=str.split(" ");
+// let arr2=[];
+// for (let index = 0; index < 3; index++) {
+//      arr2.push(array[index]);
+    
+// };
+// let result=arr2.join(" ");
+
+// console.log(result); //JavaScript is simple
+// // another way
+
+// const str = 'JavaScript is simple but not easy to master';
+// const wordLimit = 3;
+
+// function truncateWithWordLimit(str, wordLimit) {
+//    return str.split(' ').slice(0, wordLimit).join(' ');
+// }
+
+// console.log(`${truncateWithWordLimit(str, wordLimit)}`);
+
+
+// const array1 = [5, 12, 8, 130, 44];
+  
+// const found = array1.find((element) => element > 1000);
+
+// console.log(found);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
